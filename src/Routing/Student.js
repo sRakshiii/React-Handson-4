17360031d985b2405eff56d4ebf8bef9c5603842
sRakshiii <86668172+sRakshiii@users.noreply.github.com>
2023-06-01 10:07:25 +0530

    first commit

---
+++ src/Routing/Student.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from '../Navbar'
+
+export default function Student() {
+  return (
+    <>
+    <Navbar/>
+    <h1>Student Datails</h1>
+    {/* <button className="addbtn">Add New Student</button>
+    <table border={"1px"}>
+        <tr>
+            <td>Name</td>
+            <td>Age</td>
+            <td>Course</td>
+            <td>Batch</td>
+            <td>Change</td>
+        </tr>
+    </table> */}
+    </>
+  )
+}
